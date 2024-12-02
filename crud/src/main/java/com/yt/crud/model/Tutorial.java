c32ecf7783ee45a34b0909b9bc398bf7773ccf8a
Mansi Kumari <117650295+KMansi05@users.noreply.github.com>
2024-12-03 04:23:17 +0530

    Add files via upload

---
+++ crud/src/main/java/com/yt/crud/model/Tutorial.java
@@ -0,0 +1,16 @@
+package com.bezkoder.spring.data.mongodb.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tutorials")
+public class Tutorial {
+    @Id
+    private String id;
+
+    private String title;
+    private String description;
+    private boolean published;
+
+  ...
+}
